919edf76e4436ac4bfc4b21c18fc1477c5df9ee0
vziegler <vziegler@student.42wolfsburg.de>
2023-03-19 21:39:15 +0100

    20230319

---
+++ webapp/Component.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
+   "use strict";
+   return UIComponent.extend("root.Component", {
+      metadata : {
+         "rootView": {
+            "viewName": "root.view.App",
+            "type": "XML",
+            "async": true,
+            "id": "app"
+         }
+      },
+      init : function () {
+         // call the init function of the parent
+         UIComponent.prototype.init.apply(this, arguments);
+         // set data model
+         var oModel = new JSONModel({
+            recipient : {
+               name : "Nutzer"
+            }
+         });
+         this.setModel(oModel);
+         // set i18n model
+         var i18nModel = new ResourceModel({
+            bundleName: "root.i18n.i18n"
+         });
+         this.setModel(i18nModel, "i18n");
+      }
+   });
+});
--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
 	"use strict";
 
-	XMLView.create({
-		viewName: "root.view.App"
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
-
+	new ComponentContainer({
+		name: "root",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
